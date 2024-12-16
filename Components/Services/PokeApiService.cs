ad49a7a0cf978b8c0c6b536c29cf39a275a36ead
Noel Chacon <noelchacon0522@gmail.com>
2024-12-16 08:46:07 -0500

    Commit de la lista funcional con todos los pokemon cargando de forma rapida

--- Components/Services/PokeApiService.cs
+++ Components/Services/PokeApiService.cs
@@ -1,3 +1,4 @@
+using PokeApiApp.Components.Pages;
 using RestSharp;
 
 public class PokeApiService
@@ -15,10 +16,18 @@ public class PokeApiService
         var request = new RestRequest($"pokemon/{name}", Method.Get);
         return await _client.GetAsync<Pokemon>(request);
     }
+
+    public async Task<ListPokemon?> GetPokeListAsync()
+    {
+        var request = new RestRequest("pokemon?limit=100000&offset=0", Method.Get);
+        return await _client.GetAsync<ListPokemon>(request);
+    }
+
+    
     
     
 
-    // Método para obtener una página de Pokémon con solo su nombre y URL
+    /*// Método para obtener una página de Pokémon con solo su nombre y URL
     public async Task<PokemonListResponse> GetPokemonPageAsync(int page, int pageSize = 20)
     {
         int offset = (page - 1) * pageSize;
@@ -38,10 +47,28 @@ public class PokeApiService
 
         var pokemonDetails = await Task.WhenAll(tasks);
         return pokemonDetails.Where(details => details != null).ToList()!;
-    }
+    }*/
 }
 
 // Clases para representar la respuesta de la API
+
+//clases para el listado de pokemones completo
+
+public class ListPokemon
+{
+    public List<ListaInfo> Results { get; set; } = new List<ListaInfo>();
+}
+
+public class ListaInfo
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+
+
+
+
+/*
 public class PokemonListResponse
 {
     public int Count { get; set; }
@@ -52,9 +79,9 @@ public class PokemonListItem
 {
     public string Name { get; set; }
     public string Url { get; set; }
-}
-
-// Clases para los detalles de cada Pokémon
+}*/
+    
+// Clases para los detalles de cada Pokémon, primera pagina
 public class Pokemon
 {
     public int Id { get; set; }
